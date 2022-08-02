5754c6d920ec995e7a67c5e246f8be2bca93ebd5
khyunah <yd3085@naver.com>
2022-08-02 14:01:31 +0900

    카카오톡 프로필 레이아웃 따라하기

--- flutter_kakao_my/lib/components/profile_card.dart
+++ flutter_kakao_my/lib/components/profile_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kakao_my/models/user.dart';
+import 'package:flutter_kakao_my/screens/profile_screen.dart';
 
 class ProfileCard extends StatelessWidget {
   const ProfileCard({Key? key, required this.user}) : super(key: key);
@@ -12,27 +13,23 @@ class ProfileCard extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: InkWell(
         onTap: () {
-          
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ProfileScreen(user: user)),
+          );
         },
         child: ListTile(
           leading: CircleAvatar(
             radius: 20,
-            backgroundImage: NetworkImage(
-              user.backgroundImage
-            ),
+            backgroundImage: NetworkImage(user.backgroundImage),
           ),
           title: Text(
             user.name,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              fontSize: 16
-            ),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
           ),
           subtitle: Text(
             user.intro,
-            style: TextStyle(
-              fontSize: 12
-            ),
+            style: TextStyle(fontSize: 12),
           ),
           contentPadding: const EdgeInsets.all(0),
         ),
